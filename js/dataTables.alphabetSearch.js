df81f9117c6cf3dc1590fb5844bd0e928909083d
Michael Ryvkin <mpryvkin@users.noreply.github.com>
2017-06-26 08:12:24 -0400

    Version 1.2.2

--- js/dataTables.alphabetSearch.js
+++ js/dataTables.alphabetSearch.js
@@ -1,4 +1,4 @@
-/*! AlphabetSearch for DataTables v1.2.2-dev
+/*! AlphabetSearch for DataTables v1.2.2
  * 2014 SpryMedia Ltd - datatables.net/license
  * Gyrocode - MIT License
  */
@@ -7,7 +7,7 @@
  * @summary     AlphabetSearch
  * @description Show an set of alphabet buttons alongside a table providing
  *     search input options
- * @version     1.2.2-dev
+ * @version     1.2.2
  * @file        dataTables.alphabetSearch.js
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
